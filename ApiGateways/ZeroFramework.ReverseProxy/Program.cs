0febfff33bc86897fa048a6da51e5f6792147644
jiahaiyang <84194041@qq.com>
2023-11-08 11:56:23 +0800

    测试提交

--- ApiGateways/ZeroFramework.ReverseProxy/Program.cs
+++ ApiGateways/ZeroFramework.ReverseProxy/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddReverseProxy()
-.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
+
 app.MapReverseProxy();
+
 app.Run();
